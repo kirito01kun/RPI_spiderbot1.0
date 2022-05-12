c65660d88cfd1a6a9c016636d6495a5219e58114
Youssef Kbala <55252948+kirito01kun@users.noreply.github.com>
2022-05-12 01:03:52 +0100

    Add files via upload
    latest update new functions added with new moves buttons

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import os
 import time
 import pygame
-from adafruit_servokit import ServoKit
+#from adafruit_servokit import ServoKit
 from pygame.locals import *
 from colors import *
 #To choose the used kit
-kit = ServoKit(channels=16)
+#kit = ServoKit(channels=16)
 
 #kit.servo[0].angle = 180
 
 #To intiate module parts
 pygame.init()
-WIDTH, HEIGHT = 400, 400
+WIDTH, HEIGHT = 800, 400
 #To set the window...
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("SpiderBot 1.0")
+
+#Creating text surface
+
+font = pygame.font.Font('freesansbold.ttf', 13)
+txt = font.render('© 2022 | Youssef KBALA | LP : TSEI', True, WHITE, COLOR1)
+txtRect = txt.get_rect()
+txtRect.center = (118, 388)
+
 
 button_size = 85
 
@@ -28,6 +36,10 @@ down_image = pygame.image.load(os.path.join('Assets', 'DOWN.png'))
 right_image = pygame.image.load(os.path.join('Assets', 'RIGHT.png'))
 left_image = pygame.image.load(os.path.join('Assets', 'LEFT.png'))
 
+move1_image = pygame.image.load(os.path.join('Assets', 'hexagon_orange.png'))
+move2_image = pygame.image.load(os.path.join('Assets', 'hexagon_orange.png'))
+move3_image = pygame.image.load(os.path.join('Assets', 'hexagon_orange.png'))
+move4_image = pygame.image.load(os.path.join('Assets', 'hexagon_orange.png'))
 
 #spider = pygame.image.load(os.path.join('Assets', 'spider.gif'))
 #spider = pygame.transform.scale(spider, (spider_height, spider_width))# Change the size of the spider element
@@ -60,26 +72,42 @@ down_button = Button(150, 250, down_image)
 right_button = Button(250, 160, right_image)
 left_button = Button(50, 160, left_image)
 
+move1_btn = Button(560, 180, move1_image)
+move2_btn = Button(560, 55, move2_image)
+move3_btn = Button(675, 260, move3_image)
+move4_btn = Button(450, 260, move4_image)
 
 def turn_right():
     print("right")
-    kit.servo[0].angle = 180# ... 0 is the number of servo and 180 is the angle
-    kit.servo[1].angle = 180
-    kit.servo[2].angle = 90
-    kit.servo[3].angle = 90
+#    kit.servo[0].angle = 180# ... 0 is the number of servo and 180 is the angle
+#    kit.servo[1].angle = 180
+#    kit.servo[2].angle = 90
+#    kit.servo[3].angle = 90
 
 def turn_left():
     print("left")
-    kit.servo[0].angle = 0
-    kit.servo[1].angle = 0
-    kit.servo[2].angle = 0
-    kit.servo[3].angle = 0
+#    kit.servo[0].angle = 0
+#    kit.servo[1].angle = 0
+#    kit.servo[2].angle = 0
+#    kit.servo[3].angle = 0
 
 def forward():
     print("forward")
 
 def backward():
     print("backward")
+
+def move1():
+    print("move 1")
+
+def move2():
+    print("move 2")
+
+def move3():
+    print("move 3")
+
+def move4():
+    print("move 4")
 
 def game_loop():
     clock = pygame.time.Clock()
@@ -100,6 +128,7 @@ def game_loop():
                     turn_left()
 
         WIN.fill(COLOR1)
+        WIN.blit(txt, txtRect)
         if up_button.draw():
             forward()
         if down_button.draw():
@@ -108,6 +137,15 @@ def game_loop():
             turn_right()
         if left_button.draw():
             turn_left()
+        if move1_btn.draw():
+            move1()
+        if move2_btn.draw():
+            move2()
+        if move3_btn.draw():
+            move3()
+        if move4_btn.draw():
+            move4()
+        pygame.draw.rect(WIN, WHITE, SCR_BORDER)
         pygame.display.update()
 
     pygame.quit()
