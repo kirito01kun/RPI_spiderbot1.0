a69426be3e313dead5f1844afe99f4da9f89f900
Youssef Kbala <55252948+kirito01kun@users.noreply.github.com>
2022-05-22 16:37:35 +0100

    update main.py

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def ready_legs():
     #back left leg
     kit.servo[10].angle = 160
     time.sleep(1)
-    kit.servo[11].angle = 165
+    kit.servo[11].angle = 160
 
 def forward():
     print("forward")
