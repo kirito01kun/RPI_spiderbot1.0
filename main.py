18959b79f8a45210545b227318a6164fb2e63468
Youssef Kbala <55252948+kirito01kun@users.noreply.github.com>
2022-06-14 17:22:56 +0100

    update main.py

--- main.py
+++ main.py
@@ -155,77 +155,87 @@ def sit_down():
 def forward():
     print("forward")
     kit.servo[1].angle = 105#fr 2 up
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[0].angle = 170#forward rf le
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[1].angle = 75# fr2 down
-    time.sleep(0.1)
+    time.sleep(0.5)
     kit.servo[3].angle = 75#back Lf leg
+    time.sleep(0.2)
     kit.servo[6].angle = 15#back rb leg
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[10].angle = 115#lb 2 up
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[9].angle = 105#forward lb leg
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[10].angle = 85#lb 2 down
-    time.sleep(0.1)
+    time.sleep(0.5)
     # part two
     kit.servo[4].angle = 105#lf 2 up
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[3].angle = 15#forward Lf
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[4].angle = 75# lf 2 down
-    time.sleep(0.1)
+    time.sleep(0.5)
     kit.servo[0].angle = 110# back rf
+    time.sleep(0.2)
     kit.servo[9].angle = 165# back lb
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[7].angle = 120# br 2 up
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[6].angle = 85# Forward br
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[7].angle = 80# br 2 down
-    time.sleep(0.1)
+    time.sleep(0.5)
     kit.servo[0].angle = 140#Front Right 1
+    time.sleep(0.1)
     kit.servo[3].angle = 45#Front Left 1
+    time.sleep(0.1)
     kit.servo[6].angle = 45#Back Right 1
+    time.sleep(0.1)
     kit.servo[9].angle = 135#Back Left 1
     
 def backward():
     print("backward")
     kit.servo[10].angle = 115#bl 2 up
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[9].angle = 165#back bl le
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[10].angle = 85# bl2 down
-    time.sleep(0.1)
+    time.sleep(0.5)
     kit.servo[6].angle = 85#forward br leg
+    time.sleep(0.2)
     kit.servo[3].angle = 15#forward fl leg
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[1].angle = 105#fr 2 up
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[0].angle = 110#back fr leg
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[1].angle = 75#fr down
-    time.sleep(0.1)
+    time.sleep(0.5)
     # part two
     kit.servo[7].angle = 120#br 2 up
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[6].angle = 15#back br
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[7].angle = 80# br 2 down
-    time.sleep(0.1)
+    time.sleep(0.5)
     kit.servo[0].angle = 170# forward rf
+    time.sleep(0.2)
     kit.servo[9].angle = 105# forward lb
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[4].angle = 105# fl 2 up
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[3].angle = 75# back fl
-    time.sleep(0.1)
+    time.sleep(0.2)
     kit.servo[4].angle = 75# fl 2 down
-    time.sleep(0.1)
+    time.sleep(0.5)
     kit.servo[0].angle = 140#Front Right 1
+    time.sleep(0.1)
     kit.servo[3].angle = 45#Front Left 1
+    time.sleep(0.1)
     kit.servo[6].angle = 45#Back Right 1
+    time.sleep(0.1)
     kit.servo[9].angle = 135#Back Left 1
 
 def move1():
